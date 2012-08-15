53389e3b89c40ac5e7474060c26535defc5ba5db
Jon Knapp <thann@linux.com>
2012-08-14 21:34:58 -0700

    implemented basic functionality

--- phreos-terminal.py
+++ phreos-terminal.py
@@ -9,37 +9,72 @@ serverAddress = "127.0.0.1"
 
 class server_connection:
 	def __init__(self):
-		self.sock = socket.socket(self.socket.AF_INET, self.socket.SOCK_STREAM)
+		self.jobs = []
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.sock.settimeout(3)
 		try: 
 			self.sock.connect((serverAddress , port))
 		except Exception as x:
 			print "Error: Could not connect to server."
+			sys.exit(1)
 		
 	def auth_user(self, user, passwd):
-		self.sock.send("authuser="+username+":"+passwd+"~")
+		self.sock.send("authuser="+user+":"+passwd+"~")
+		string = self.sock.recv(1024)
+		return True if string == "success" else False
 	
 	def query_printers(self):
-		return ["NOT YET IMPLEMENTED"]
+		self.sock.send("request_list")
+		self.jobs = []
+		string = ""
+		while(True):
+			string = self.sock.recv(1024)
+			if string == "done":
+				break
+			print "receivedQQ: ", string
+			self.sock.send("next")
+			self.jobs.append(string.split(";"))
+		return self.jobs
 		
 	def release_job(self, num):
-		pass
+		if num >= len(self.jobs):
+			print "ERROR: Bad Job number!"
+			return False
+		num = self.jobs[num][3]
+		self.sock.send("release="+str(num))
+		string = self.sock.recv(1024)
+		return True if string == "success" else False
+
 	def cancel_job(self, num):
-		pass
+		if num >= len(self.jobs):
+			print "ERROR: Bad Job number!"
+			return False
+		num = self.jobs[num][3]
+		self.sock.send("cancel="+str(num))
+		string = self.sock.recv(1024)
+		return True if string == "success" else False
+	
+	def close(self):
+		#self.sock.send("kill")
+		self.sock.close()
 
 def main_simple_CLI():
-	print "print which one:"
-	
-	con = cups_LP()
-	con.auth_user("jon","tester")
-	printer_list = con.query_printers()
+	conn = server_connection()
+	conn.auth_user("jon","tester")
+	printer_list = conn.query_printers()
 	for p in printer_list:
 		print p
+	#~ print printer_list
 	
 	print "print which one:"
 	sin = int(sys.stdin.readline())-1
 	
-	backend.release_job(sin)
+	#ret = conn.release_job(sin)
+	ret = conn.cancel_job(sin)
+	print "success?",ret
+
+	conn.close()
+	
 
 if __name__ == "__main__":
 	main_simple_CLI()
--- phreos.py
+++ phreos.py
@@ -17,7 +17,16 @@ class cups_LP:
 		self.spool = "/var/spool/cups/"
 		self.printer = "Virtual_PDF_Printer"
 		self.lpq = []
-	
+		
+	#Translates cups-job-num to lpq-index-num
+	def _get_job_num(self, num):
+		for index, job in enumerate(self.lpq):
+			if job[3] == str(num) :
+				return index
+
+		print "ERROR: not a valid job number!"
+		return -1
+
 	def query_printers(self):
 		lpq = subprocess.Popen("lpq -P "+self.printer, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		lpq = lpq.stdout.readlines()[2:]
@@ -28,27 +37,25 @@ class cups_LP:
 			spoolNum = "d%05.0f-001" % (spoolNum)
 			pageNum = subprocess.Popen("pkpgcounter "+self.spool+spoolNum, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 			pageNum = int(pageNum.stdout.readline())
-			lpq[index].insert(1,pageNum)
+			lpq[index].insert(1,str(pageNum))
 			lpq[index] = lpq[index][:-2]
 		self.lpq = lpq
 		return self.lpq
 
 	def release_job(self, num):
-		print "NUM = "+str(num)
-		if num >= len(self.lpq) or num < 0 :
-			print "ERROR: not a valid job number!"
+		num = self._get_job_num(num)
+		if num < 0:
 			return False
 		print "releaseing job num "+self.lpq[num][3]
-		subprocess.Popen("lp -i "+self.printer+"-"+self.lpq[num][3]+" -H immediate", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		#subprocess.Popen("lp -i "+self.printer+"-"+self.lpq[num][3]+" -H immediate", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		return True
 		
 	def cancel_job(self, num):
-		print "NUM = "+str(num)
-		if num >= len(self.lpq) or num < 0 :
-			print "ERROR: not a valid job number!"
+		num = self._get_job_num(num)
+		if num < 0:
 			return False
 		print "canceling job num "+self.lpq[num][3]
-		subprocess.Popen("cancel "+self.printer+" "+self.lpq[num][3], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		#subprocess.Popen("cancel "+self.printer+" "+self.lpq[num][3], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		return True
 		
 class logger:
@@ -56,56 +63,80 @@ class logger:
 		self.logFile = open(filename,'a')
 	
 	def add(self, entry):
-		logFile.write(datetime.datetime+" : "+entry)
+		self.logFile.write(str(datetime.datetime.today())+" : "+entry+"\n")
 		
 class service:
 	def __init__(self):
-		backend = cups_LP()
-		log = logger("phreos_print.log")		
+		self.backend = cups_LP()
+		self.log = logger("phreos_print.log")		
 		
-	def handleClient( clientSock, address ):
-		authInfo = []
+	def handleClient(self, clientSock, address ):
+		string = ""
+		authInfo = None
 		while (string != "kill"):
 			try:
 				string = clientSock.recv(1024)
-				print "received: ", string
+				print "received: >"+string+"<"
 				
-				if (string[:9] == "authuser=")
-					authInfo = string[9:].split(":")
+				if (string[:9] == "authuser="):
+					authInfo = dict(zip(["user","passwd"],string[9:].split(":")))
+					print authInfo
+					clientSock.send("success")
 				
 				elif (string == "request_list"):
-					jobs = backend.query_printers()
-					for line in jobs
+					jobs = self.backend.query_printers()
+					for line in jobs:
+						print "LINE:",line
 						s = ";".join(line)
 						clientSock.send(s)
+						clientSock.recv(1024)
 					clientSock.send("done")
 					
 				elif (string[:8] == "release="):
 					if len(authInfo):
-						backend.release_job(int(string[8:]))
-						log.add(user+" printed job# "+string[8:])
+						r = self.backend.release_job(int(string[8:]))
+						if r:
+							self.log.add(authInfo["user"]+" printed job# "+string[8:])
+							clientSock.send("success")
+						else:
+							print "ERROR: Unable to release job."
+							clientSock.send("ERROR")
 					else:
-						print "ERROR: tried to print before Auth."
-						string = "kill"
+						print "ERROR: Tried to print before Auth."
+						break
+						
+				elif (string[:7] == "cancel="):
+					if len(authInfo):
+						r = self.backend.cancel_job(int(string[7:]))
+						if r:
+							self.log.add(authInfo["user"]+" canceled job# "+string[7:])
+							clientSock.send("success")
+						else:
+							print "ERROR: Unable to cancel job."
+							clientSock.send("ERROR")
+					else:
+						print "ERROR: Tried to cancel before Auth."
+						break
 				
 				else:
 					print "received bad string: "+string
-					string = "kill"
+					break
+					
 			except Exception as x:
-				print "ERROR: "+x
-				string = "kill"
+				print "ERROR:",x
+				break
 		
 		##Gracefully disconnect
-	
-	def serviceLoop(servSock):
+
+	def serviceLoop(self, servSock):
 		while 1:
 			try:
 				print "Waiting for next client..."
 				(clientSock, address) = servSock.accept()
 				print "Client Connected, Creating Thread"
-				threading.Thread(target=handleClient, args=(clientSock, address)).start()
+				threading.Thread(target=self.handleClient, args=(clientSock, address)).start()
 			except Exception as x:
-				print "Exiting Service."
+				print "ERROR:",x,"\nExiting Service."
 				servSock.close()
 				break	
 		
@@ -114,7 +145,7 @@ class service:
 		servSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		servSock.bind(('', port))
 		servSock.listen(maxClients)
-		serviceLoop(servSock)
+		self.serviceLoop(servSock)
 
 def main_simple_CLI():
 	backend = cups_LP()
@@ -128,4 +159,6 @@ def main_simple_CLI():
 	backend.release_job(sin)
 
 if __name__ == "__main__":
-	main_simple_CLI()
+	#main_simple_CLI()
+	serv = service()
+	serv.start()
